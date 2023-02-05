94f4b620e10f1ec1395f8e26d9ccc0758dab24a5
Laurin49 <welter@diwe.info>
2023-02-05 19:28:07 +0100

    dev03 Einsendeaufgabe 01 - YSQLD14

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarsController;
 use App\Http\Controllers\ReservationsController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ContractsController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', function () { return view('welcome'); });
+
 Route::get('/cars', [CarsController::class, 'index'] );
 Route::get('/reservations', [ReservationsController::class, 'index'] );
+Route::get('/contracts', [ContractsController::class, 'index'] );
